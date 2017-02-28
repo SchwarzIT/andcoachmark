fbe547984906b0bbb7527bd136dd1bfc846e195a
Michael Link <michael.link@kaufland.com>
2017-02-28 19:09:42 +0100

    # organized imports, formatted code

--- demo/src/main/java/kaufland/com/demo/DemoAdapter.java
+++ demo/src/main/java/kaufland/com/demo/DemoAdapter.java
@@ -6,17 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.MyViewHolder>  {
+public class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.MyViewHolder> {
 
     private String[] mItems;
 
     private DemoClickListener mClickListener;
 
     DemoAdapter() {
-        mItems = new String[]{"1","2","3","4","2","3","4","2","3","4","2","3","4","2","3","4","2","3","4","2","3","4","2","3","4","2","3","4","2","3","4",};
+        mItems = new String[]{"1", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4", "2", "3", "4",};
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
